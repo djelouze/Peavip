5dbaae57b3cddabdd74438e724d7fccfd6b473ea
jeromevelut <jerome.velut@gmail.com>
2009-12-03 17:06:31 +0100

    [ENH] Changing tree structure.
    For now, "Natives" folder is not considered by CMake. It will, soon...

---
+++ Natives/Sources/add_source.sh
@@ -0,0 +1,13 @@
+echo "<ServerManagerConfiguration>
+   <ProxyGroup name=\"sources\">
+      <SourceProxy name=\"$1\"
+                   class=\"vtk$1\"
+                  label=\"\"
+         <Documentation>
+                      long_help=\"\" 
+                      short_help=\"\"> 
+         </Documentation> 
+      </SourceProxy>
+   </ProxyGroup> 
+</ServerManagerConfiguration>" > $1.xml 
+
